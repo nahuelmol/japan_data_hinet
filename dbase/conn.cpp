49b5b7b372e475d2d8d3c16abe5ca2440b036378
unknown <molinahue44@gmail.com>
2025-05-27 20:03:15 -0300

    sqlite3 database connection established;

--- dbase/conn.cpp
+++ dbase/conn.cpp
@@ -1,3 +1,17 @@
 //here should be a connection to a database (sql)
+#include <iostream>
+extern "C" {
+#include "sqlite3.h"
+}
 
+void connect() {
+    sqlite3* db;
+    int rc = sqlite3_open("dbase.db", &db);
+    if(rc != SQLITE_OK) {
+        std::cerr << "err: " << sqlite3_errmsg(db) << std::endl;
+    } else {
+        std::cout << "db connected" << std::endl;
+    }
+    sqlite3_close(db);
+}
 
--- dbase/queries.cpp
+++ dbase/queries.cpp
@@ -1,12 +1,20 @@
 
-
-void insert() {
-    "INSERT INTO "
-    "VALUES ("
-    ");"
+std::string insert_earthquake(std::string ename) {
+    std::string query = "INSERT INTO EARTHQUAKES VALUES (earthquakename " + ename + " TEXT);"
+    return query;
 }
 
+std::string select_all(std::string tablename){
+    std::string query = "SELECT * FROM " + tablename + ";";
+    return query;
+}
 
-void select_all(std::string tablename){
-    std::string query = "SELECT * FROM " + ";";
+std::string select_earthquake(std::string earthquakename){
+    std::string query = "SELECT * FROM earthquakes WHERE earthquakename="+earthquake";";
+    return query;
+}
+
+std::string delete_record(std::string tablename, int id){
+    std::string query = "DELETE * FROM " + tablename + "WHERE id=" + id + ");";
+    return query;
 }
